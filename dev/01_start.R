83591978aac39e4a417e3866f64e0fdd734d3f97
Fernando INVICO <fcorrales@invico.gov.ar>
2021-10-21 12:18:29 -0300

    Complete Set Up of R INVICO

--- dev/01_start.R
+++ dev/01_start.R
@@ -21,11 +21,13 @@
 golem::fill_desc(
   pkg_name = "invicoctrlr", # The Name of the package containing the App
   pkg_title = "R INVICO", # The Title of the package containing the App
-  pkg_description = "PKG_DESC.", # The Description of the package containing the App
+  pkg_description = "Shiny App develop to control INVICO’s
+  (Instituto de Vivienda de Corrientes) budget execution.
+  The database it's not provide within this package due to privacy reasons.", # The Description of the package containing the App
   author_first_name = "Fernando", # Your First Name
   author_last_name = "Corrales", # Your Last Name
   author_email = "fscorrales@gmail.com", # Your Email
-  repo_url = NULL # The URL of the GitHub Repo (optional)
+  repo_url = "https://github.com/fscorrales/invicoctrlr.git" # The URL of the GitHub Repo (optional)
 )
 
 ## Set {golem} options ----
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(invicoctrlr)
+
+test_check("invicoctrlr")
